9abe9932a13f66fca548fcc849816c6e41a41afe
Hansgill7 <200416303@student.georgianc.on.ca>
2020-11-28 18:39:05 -0500

    Create Lab3script.ps1

---
+++ powershell/Lab3script.ps1
@@ -0,0 +1,3 @@
+﻿$adapters = get-ciminstance win32_networkadapterconfiguration
+$filteredadapters = $adapters | Where-Object {$_.IPEnabled}
+$filteredadapters | format-table -autosize description,index,ipsubnet,dnsserversearchorder,dnsdomain,ipaddress
